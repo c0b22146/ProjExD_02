cb23c96124dad63918397023504c0af0dc68ea1b
seita <c0b22146af@edu.teu.ac.jp>
2023-06-20 14:10:38 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,21 +14,25 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
-    enn = pg.Surface((20,20))
+    enn = pg.Surface((20,20)) #練習1
     pg.draw.circle(enn,(255,0,0),(10,10),10)
     enn.set_colorkey((0,0,0))
+    # 黒い部分の透明化
+    xx = random.randint(0,WIDTH)
+    yy = random.randint(0,HEIGHT)
+    # 円の座標の乱数
+    enn_rct = enn.get_rect()
+    enn_rct.center = xx,yy
+    # 円の座標の抽出
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        xx = random.randint(0,1600)
-        yy = random.randint(0,900)
-        enn_rct = enn.get_rect()
-        enn_rct.center = xx,yy
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
         screen.blit(enn,enn_rct)
+        # 円の描画
         pg.display.update()
         tmr += 1
         clock.tick(10)
